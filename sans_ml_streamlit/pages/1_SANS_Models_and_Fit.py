4a6f97aaa3b71dcda9446192213ef134a76e5429
criosx <mail@frank-heinrich.net>
2023-05-24 15:55:34 -0400

    Finished parameter editing to the SANS Models tab.

--- sans_ml_streamlit/pages/1_SANS_Models_and_Fit.py
+++ sans_ml_streamlit/pages/1_SANS_Models_and_Fit.py
@@ -11,17 +11,33 @@ import subprocess
 user_sans_model_dir = st.session_state['user_sans_model_dir']
 user_sans_file_dir = st.session_state['user_sans_file_dir']
 user_sans_fit_dir = st.session_state['user_sans_fit_dir']
+user_sans_temp_dir = os.path.join(st.session_state['streamlit_dir'], 'temp')
 
 
 # ---- Functionality --------
-def get_info_from_runfile(model_name):
+def add_par(parname, model_name, fitobj):
+    fitobj.Interactor.fnReplaceParameterLimitsInSetup(parname, 0., 1., modify='add')
+    shutil.copyfile(os.path.join(user_sans_temp_dir, model_name), os.path.join(user_sans_model_dir, model_name))
 
-    fitdir = os.path.join(st.session_state['streamlit_dir'], 'temp')
+
+def remove_par(parname, model_name, fitobj):
+    fitobj.Interactor.fnReplaceParameterLimitsInSetup(parname, 0., 1., modify='remove')
+    shutil.copyfile(os.path.join(user_sans_temp_dir, model_name), os.path.join(user_sans_model_dir, model_name))
+
+
+def update_par(df_pars, model_name, fitobj):
+    labels = list(df_pars.index)
+    print(labels)
+    print(df_pars)
+    for index, row in df_pars.iterrows():
+        fitobj.Interactor.fnReplaceParameterLimitsInSetup(index, row['lowerlimit'], row['upperlimit'])
+    shutil.copyfile(os.path.join(user_sans_temp_dir, model_name), os.path.join(user_sans_model_dir, model_name))
+
+def get_info_from_runfile(model_name):
+    fitdir = user_sans_temp_dir
     runfile = os.path.join(user_sans_model_dir, model_name)
     # extract name of data files from runfile
     datafile_names = api_sasview.extract_data_filenames_from_runfile(runfile=runfile)
-    # determine, which parameters are fittable
-    # fit_row = api_sasview.extract_parameters_from_runfile(runfile=runfile)
 
     # strip any long path from filename and retain only the basename, write back to file
     datafile_names = [os.path.basename(file) for file in datafile_names]
@@ -57,11 +73,22 @@ def get_info_from_runfile(model_name):
         problem=None,
     )
 
-    df = pandas.DataFrame.from_dict(fitobj.fnLoadParameters())
-    df = df.drop(index=['number', 'relval', 'variable', 'error'])
-    # df.loc['fit'] = fit_row
-    os.chdir(olddir)
-    return df, datafile_names
+    df_pars = pandas.DataFrame.from_dict(fitobj.fnLoadParameters())
+    df_pars = df_pars.drop(index=['number', 'relval', 'variable', 'error', 'value'])
+    df_pars = df_pars.transpose()
+    li_allpars = list(fitobj.Interactor.problem.model_parameters().keys())
+    # os.chdir(olddir)
+    return df_pars, li_allpars, datafile_names, fitobj
+
+
+@st.cache_data
+def load_model(file):
+    try:
+        with open(os.path.join(user_sans_model_dir, file.name), "wb") as f:
+            f.write(file.getbuffer())
+        st.session_state['sans_model_selectbox'] = file.name
+    except IOError:
+        pass
 
 
 @st.cache_data
@@ -132,41 +159,82 @@ def save_sans_model(path, name):
 
 # --- GUI ----------------------
 st.write("""
-# Setup SANS Models
+# Select SANS Model
 """)
 
 # -------- SANS model loader
 model_path = user_sans_model_dir
 model_list = os.listdir(model_path)
 model_list = [element for element in model_list if '.py' in element]
-model_name = st.selectbox("SANS model", model_list)
+
+# st.text("Select SANS model")
+col1_a, col1_b = st.columns([1, 2])
+uploaded_model = col1_b.file_uploader("Upload", type=['py'])
+if uploaded_model is not None:
+    load_model(uploaded_model)
+model_name = col1_a.selectbox("Select existing", model_list, key='sans_model_selectbox')
 
 if model_name:
+    with open(os.path.join(user_sans_model_dir, model_name), "rb") as file:
+        btn = col1_a.download_button(
+            label="Download Model",
+            data=file,
+            file_name=model_name,
+            mime='text/plain'
+        )
+
+    st.divider()
+    st.write("""
+    # Edit Model
+    """)
+
     with open(os.path.join(model_path, model_name)) as f:
         model_txt = f.readlines()
-
-    with st.expander("Model Script"):
+    with st.expander("Edit Model Script"):
         txt = st.text_area('Model Script', "".join(model_txt), key='sans_model_text', height=400,
                            on_change=save_sans_model, args=[model_path, model_name], label_visibility='collapsed')
 
-    parameters, datafile_names = get_info_from_runfile(model_name)
+    col1_1, col1_2 = st.columns([1, 1])
+    df_pars, li_all_pars, datafile_names, model_fitobj = get_info_from_runfile(model_name)
+    #col1_1.text('Edit fit ranges')
+    parameters_edited = col1_1.experimental_data_editor(df_pars)
+    col1_1.button('Apply', on_click=update_par, args=[parameters_edited, model_name, model_fitobj],
+                  use_container_width=False)
 
-    parameters_edited = st.experimental_data_editor(parameters)
+    #col1_2.text('Add fit parameter')
+    col1_2_1, col1_2_2 = col1_2.columns([2, 1])
+    li_current_pars = sorted(list(df_pars.index.values))
+    li_addable_pars = sorted(list(set(li_all_pars) - set(li_current_pars)))
+    par_to_add = col1_2_1.selectbox("Add fit parameter", li_addable_pars, label_visibility='collapsed')
+    col1_2_2.button('Add', on_click=add_par, args=[par_to_add, model_name, model_fitobj], use_container_width=True)
+
+    #col1_2.text('Remove fit parameter')
+    col1_2_3, col1_2_4 = col1_2.columns([2, 1])
+    par_to_del = col1_2_3.selectbox("Remove fit parameter", li_current_pars, label_visibility='collapsed')
+    col1_2_4.button('Remove', on_click=remove_par, args=[par_to_del, model_name, model_fitobj],
+                    use_container_width=True)
+
+    col1_2.text('Expected Data Files')
+    for file in datafile_names:
+        col1_2.text("  " + file)
     # TODO implement back-propagation to script
 
     st.divider()
 
 
-st.write('Fit Data')
+st.write("""
+# Fit Data
+""")
+
 uploaded_file = st.file_uploader("Upload SANS files specified in your script", accept_multiple_files=True)
 if uploaded_file is not None:
     if not isinstance(uploaded_file, list):
         filelist = [uploaded_file]
     load_sans_files(uploaded_file)
 
-col1_1, col1_2 = st.columns([1, 1])
-burn = col1_1.number_input('burn', format='%i', step=50, min_value=50, value=100, key='burn')
-steps = col1_2.number_input('steps', format='%i', step=50, min_value=50, value=100, key='steps')
+col1_3, col1_4 = st.columns([1, 1])
+burn = col1_3.number_input('burn', format='%i', step=50, min_value=50, value=100, key='burn')
+steps = col1_4.number_input('steps', format='%i', step=50, min_value=50, value=100, key='steps')
 
 if st.button('Run Fit'):
     if model_name is not None and uploaded_file is not None:
